5cff8f6397212456f467d0c97cdd4d03a6dff8ff
Matthew Penney <matt@matthewpenney.net>
2026-06-07 13:41:02 +0100

    Add instructions and IPC metrics to group

--- core/metric_grp/metric_grps/branch.c
+++ core/metric_grp/metric_grps/branch.c
@@ -2,15 +2,17 @@
 
 static metric_id_t metrics[] = {
     METRIC_CPU_CYCLES,
+    METRIC_INSTRUCTIONS,
     //METRIC_BRANCH_INSTRUCTIONS,
     METRIC_BRANCH_MISPREDICTIONS,
     //METRIC_BRANCH_MISPRED_RATE,
+    METRIC_INSTRUCTIONS_PER_CYCLE,
 };
 
 static metric_grp_t mg = {
     .name = "BRANCH",
     .backend = METRIC_BE_PERF,
-    .n_metrics = 2,
+    .n_metrics = 4,
     .metrics = metrics,
 };
 
